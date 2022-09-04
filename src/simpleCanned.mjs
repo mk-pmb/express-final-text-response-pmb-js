bcdf623adb164164757848e9191ecbcb2ff53e56
mk-pmb <mk-pmb@users.noreply.github.com>
2022-09-04 21:20:29 +0200

    v0.1.4: throwable: Add option explain

--- src/simpleCanned.mjs
+++ src/simpleCanned.mjs
@@ -1,6 +1,7 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
 import isFunc from 'is-fn';
+import isStr from 'is-string';
 
 import installApi from './installApi.mjs';
 
@@ -36,15 +37,16 @@ function explain(sc, detail) {
 }
 
 
-function throwable(sc) {
-  const { opt } = sc;
-  const {
-    text,
-    ...other
-  } = opt;
-  delete other.ftr;
-  delete other.type;
-  return Object.assign(new Error(text), throwable.defaultProps, other);
+function throwable(sc, arg) {
+  if (arg && isStr(arg)) { return throwable(sc, { explain: arg }); }
+  const opt = { ...sc.opt, ...arg };
+  let msg = opt.text;
+  delete opt.text;
+  if (opt.explain) { msg += ': ' + opt.explain; }
+  delete opt.explain;
+  delete opt.ftr;
+  delete opt.type;
+  return Object.assign(new Error(msg), throwable.defaultProps, opt);
 }
 
 throwable.defaultProps = {
