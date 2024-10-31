0a5d25b79a3cb1d4e8838fe50a43c3beb40ca47b
mk-pmb <mk-pmb@users.noreply.github.com>
2024-10-31 10:33:27 +0100

    v0.1.14: When censoring an error, log a UUID ref.

--- extras/handleUnknownError.mjs
+++ extras/handleUnknownError.mjs
@@ -1,5 +1,7 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import randomUuid from 'uuid-random';
+
 import guessHttpStatusCode from '../src/guessHttpStatusCode.mjs';
 
 
@@ -8,6 +10,8 @@ const EX = function handleUnknownError(ftr, err, req, res, next) {
   if (!err) { return next(); }
   let logVerb = 'Too late to serve';
   let reply;
+  const { logHUnkErr, genUnkErrRef } = ftr.cfg;
+  let logRef = null;
   if (!req.res.finished) {
     const code = guessHttpStatusCode(err);
     if (code) {
@@ -15,14 +19,17 @@ const EX = function handleUnknownError(ftr, err, req, res, next) {
       logVerb = 'Serve';
     } else {
       reply = { code: 500, text: 'Internal Server Error' };
+      if (logHUnkErr) {
+        logRef = (genUnkErrRef || randomUuid)();
+        reply.text += ' | ref:' + logRef;
+      }
       logVerb = 'Censor';
     }
   }
 
-  const { logHUnkErr } = ftr.cfg;
   if (logHUnkErr) {
     const msg = logVerb + ' error message for';
-    logHUnkErr({ logVerb, msg, err, req });
+    logHUnkErr({ logVerb, msg, err, req, logRef });
   }
 
   if (reply) { ftr(req, reply); }
